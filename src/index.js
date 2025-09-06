ade698969d76ca3048bb0832badc65a36efc2287
seungjoonH <hsj6831@gmail.com>
2025-09-06 17:15:42 +0900

    refactor: 메인 엔트리포인트 파일 삭제

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { DiffCollector } from "./collect.js";
-
-export async function run() {
-  const cwd = process.cwd();
-  const dc = new DiffCollector({ cwd });
-  const text = await dc.collectDiffText();
-  if (!text) { console.log("[acommit] 변경 없음"); return; }
-  console.log(text);
-}
